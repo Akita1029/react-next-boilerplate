ccee9fba00a7b1b1e94a40147e7224b55f95a17a
akita <akita@1029.com>
2023-02-01 05:27:37 +0900

    addon pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-module.exports = {
+// module.exports = {
+//   i18n: {
+//     locales: ['en', 'es'],
+//     defaultLocale: 'en',
+//   },
+// };
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = withPWA({
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
   },
-};
+})
